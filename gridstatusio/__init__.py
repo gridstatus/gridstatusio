269c4a3369ebb45e7b73b12a45b7de1b17dc6763
Max Kanter <kmax12@gmail.com>
2023-08-23 21:16:28 -0500

    Check if update available at import

--- gridstatusio/__init__.py
+++ gridstatusio/__init__.py
@@ -1,3 +1,6 @@
-from gridstatusio.version import __version__
+from gridstatusio.version import __version__, check_for_update
 
 from gridstatusio.gs_client import GridStatusClient
+
+
+check_for_update()
